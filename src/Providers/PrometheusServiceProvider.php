ddd49903bca61aff7fb6765c663dbf6d13daaa62
Mehran Niknafs <mehran.niknafs@gmail.com>
2021-05-16 09:46:04 +0430

    Add global middleware to laravel

--- src/Providers/PrometheusServiceProvider.php
+++ src/Providers/PrometheusServiceProvider.php
@@ -12,6 +12,11 @@ class PrometheusServiceProvider extends ServiceProvider
             __DIR__ . '/../config/prometheus.php' => $this->configPath('prometheus.php'),
         ]);
         $this->loadRoutesFrom(__DIR__ . '/../../routes/routes.php');
+        
+        if (app() instanceof \Illuminate\Foundation\Application) {
+            $kernel = $this->app->make(\Illuminate\Contracts\Http\Kernel::class);
+            $kernel->pushMiddleware(PrometheusMiddleware::class);
+        }
 
     }
 
